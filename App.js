46657cfc9f48e2260c8b55de2cf298058e686594
irene-poxylari <49757176+irene-poxylari@users.noreply.github.com>
2021-11-23 16:09:51 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import { Table } from "./components/Table";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <Table />
+    </div>
+  );
+}
+
+export default App;
